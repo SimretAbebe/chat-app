0e0f204dfe3831816968efc92b1e5f1454725cbc
SimretAbebe <simretabebe24@gmail.com>
2025-08-06 18:36:51 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV ==="production"){
 
 
 
-    app.get("*/splat", (req, res) => {
+    app.get(/(.*)/, (req, res) => {
       res.sendFile(
         path.join(__dirname, "../frontend/frontend", "dist", "index.html")
       );
